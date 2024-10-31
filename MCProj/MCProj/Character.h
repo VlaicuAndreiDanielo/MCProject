f65c156bc22c9c6be096980ae1b73cc9d9e93ffc
VlaicuAndreiDanielo <169060199+VlaicuAndreiDanielo@users.noreply.github.com>
2024-10-31 14:33:19 +0200

    Created Charater class

---
+++ MCProj/MCProj/Character.h
@@ -0,0 +1,17 @@
+#pragma once
+class Character
+{
+public:
+	Character();
+	Character(int hp, int speed);
+	virtual void attack() const;
+
+	virtual void activateSpecialAbility() const = 0;
+
+	virtual ~Character() = default;
+
+private:
+	int HP;
+	int speed;
+};
+

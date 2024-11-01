47da1e7a0137b84a69d4272c36bef153a67b4168
Mario <mariovoicuandrei@gmail.com>
2024-11-01 17:27:23 +0200

    Added constant default values for weapons

---
+++ TheMonkeyBusyness/TheMonkeyBusyness/weaponsConfig.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace WeaponConfig
+{
+    const int BASIC_BULLET_DAMAGE = 10;
+    const float BASIC_BULLET_SPEED = 1.0;
+    const float BASIC_FIRE_RATE = 1;
+
+    // Berry Blaster
+    const int BERRY_BLASTER_DAMAGE = 5;
+    const float BERRY_BLASTER_SPEED = 2;
+    const float BERRY_BLASTER_FIRE_RATE = 3;
+    const int BERRY_BLASTER_BULLET_COUNT = 30;
+
+    // Coconut Cannon
+    const int COCONUT_CANNON_DAMAGE = 30;
+    const float COCONUT_CANNON_SPEED = 1.0;
+    const float COCONUT_CANNON_FIRE_RATE = 0.5;
+    const int COCONUT_CANNON_BULLET_COUNT = 5;
+
+    // Pineapple Grenade
+    const int PINEAPPLE_GRENADE_DAMAGE = 20;
+    const float PINEAPPLE_GRENADE_SPEED = 1.5;
+    const float PINEAPPLE_GRENADE_FIRE_RATE = 1.0;
+    const int PINEAPPLE_GRENADE_BULLET_COUNT = 5;
+
+    // Tropical Thunder
+    const int TROPICAL_THUNDER_DAMAGE = 10;
+    const float TROPICAL_THUNDER_SPEED = 0;  // instant
+    const float TROPICAL_THUNDER_FIRE_RATE = 0; //single use so it doesn't matter
+    const float TROPICAL_THUNDER_BULLET_COUNT = 1;
+}

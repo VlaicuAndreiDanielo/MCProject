d98d139ab64198851cf321ededd8332c19f53000
Arsene Tudor <tudor.arsene@student.unitbv.ro>
2024-12-01 17:19:02 +0200

    Added isPassable and isShootable properties for all tile types

--- TheMonkeyBusyness/TheMonkeyBusyness/Tile.cpp
+++ TheMonkeyBusyness/TheMonkeyBusyness/Tile.cpp
@@ -1,58 +1,92 @@
 #include "Tile.h"
 #include <iostream>
 
-Tile::Tile(TileType type) : m_tileType{ type }, m_hp{ 0 }, m_playerOccupied{ false } {
-	if (type == TileType::DestructibleWall) {
-		m_hp = 3; // Default HP for destructible walls
-	}
+Tile::Tile(TileType type)
+    : m_tileType{ type }, m_hp{ 0 }, m_playerOccupied{ false }, m_isPassable{ true }, m_isShootable{ true } {
+    if (type == TileType::DestructibleWall) {
+        m_hp = 3; // Default HP for destructible walls
+    }
+    updateProperties();
 }
-Tile::~Tile()
-{
+
+Tile::~Tile() {
 
 }
+
 void Tile::setType(TileType type) {
-	m_tileType = type;
-	if (type == TileType::DestructibleWall) {
-		m_hp = 3; // Reset HP for destructible walls
-	}
-	else {
-		m_hp = 0; // Other tiles have no HP
-	}
+    m_tileType = type;
+    if (type == TileType::DestructibleWall) {
+        m_hp = 3; // Reset HP for destructible walls
+    }
+    else {
+        m_hp = 0; // Other tiles have no HP
+    }
+    updateProperties(); // Adjust passable and shootable properties
 }
 
-TileType Tile::getType() const 
-{ 
-	return m_tileType; 
+TileType Tile::getType() const {
+    return m_tileType;
 }
 
-void Tile::setOccupied(bool occ)
-{
-	this->m_playerOccupied = occ;
+void Tile::setOccupied(bool occ) {
+    this->m_playerOccupied = occ;
 }
 
-bool Tile::getOccupied() const
-{
-	return m_playerOccupied;
+bool Tile::getOccupied() const {
+    return m_playerOccupied;
 }
 
-// Methods for managing HP
 void Tile::setHP(int health) {
-	m_hp = health;
+    m_hp = health;
 }
 
 int Tile::getHP() const {
-	return m_hp;
+    return m_hp;
 }
 
 void Tile::takeDamage(int damage) {
-	if (m_tileType == TileType::DestructibleWall || m_tileType == TileType::FakeDestructibleWall) {
-		m_hp -= damage;
-		if (m_hp <= 0) {
-			if (m_tileType == TileType::FakeDestructibleWall) {
-				// Trigger explosion (logic to be implemented elsewhere)
-				std::cout << "Fake wall exploded!\n";
-			}
-			m_tileType = TileType::Empty; // Turn into empty tile
-		}
-	}
+    if (m_tileType == TileType::DestructibleWall || m_tileType == TileType::FakeDestructibleWall) {
+        m_hp -= damage;
+        if (m_hp <= 0) {
+            if (m_tileType == TileType::FakeDestructibleWall) {
+                // Trigger explosion (logic to be implemented elsewhere)
+                std::cout << "Fake wall exploded!\n";
+            }
+            m_tileType = TileType::Empty; // Turn into empty tile
+            updateProperties(); // Update properties after type change
+        }
+    }
+}
+
+void Tile::updateProperties() {
+    switch (m_tileType) {
+    case TileType::Empty:
+    case TileType::Grass:
+        m_isPassable = true;
+        m_isShootable = true;
+        break;
+    case TileType::DestructibleWall:
+        m_isPassable = false;
+        m_isShootable = false;
+        break;
+    case TileType::FakeDestructibleWall:
+        m_isPassable = false;
+        m_isShootable = false;
+        break;
+    case TileType::IndestructibleWall:
+        m_isPassable = false;
+        m_isShootable = false;
+        break;
+    case TileType::Water:
+        m_isPassable = false;
+        m_isShootable = true;
+        break;
+    case TileType::Lava:
+        m_isPassable = false;
+        m_isShootable = true;
+        break;
+    default:
+        m_isPassable = true;
+        m_isShootable = true;
+    }
 }
--- TheMonkeyBusyness/TheMonkeyBusyness/Tile.h
+++ TheMonkeyBusyness/TheMonkeyBusyness/Tile.h
@@ -8,6 +8,8 @@ private:
     TileType m_tileType;
     int m_hp; // Health for destructible walls
     bool m_playerOccupied;
+    bool m_isPassable;  // Can units walk through this tile?
+    bool m_isShootable; // Can bullets or projectiles pass through this tile?
 
 public:
     Tile(TileType type);
@@ -22,5 +24,8 @@ public:
     void setHP(int health);
     int getHP() const;
     void takeDamage(int damage); // Decrease HP
+
+private:
+    void updateProperties(); // Automatically adjust properties based on tile type
 };
 

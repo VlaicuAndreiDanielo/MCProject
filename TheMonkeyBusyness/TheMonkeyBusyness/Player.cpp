657c4cb7f31d6f14595737040e810f32d37cc418
Mario <mariovoicuandrei@gmail.com>
2024-12-01 20:20:22 +0200

    Add logic to player for moving to server and serialize to json

--- TheMonkeyBusyness/TheMonkeyBusyness/Player.cpp
+++ TheMonkeyBusyness/TheMonkeyBusyness/Player.cpp
@@ -1,37 +1,17 @@
 #include "Player.h"
 #include "iostream"
 
-Player::Player(float x, float y) //sets spawn of the player 
+Player::Player(float x, float y) : m_id{ PlayerConfig::kDefaultPlayeId} //sets spawn of the player 
 {
 	m_Character = new BasicMonkey();
 	m_position.x = x;
 	m_position.y = y;
 }
 
-
 Vector2 Player::Forward()
 const {
 	return this->m_direction;
 }
-
-//void Player::draw(QPainter& painter) const
-//{
-//	painter.save();
-//
-//	// Translate and rotate to draw the player at the correct position and angle
-//	painter.translate(m_position.x, m_position.y);
-//	painter.rotate(m_rotationAngle);
-//
-//	painter.setBrush(Qt::white);
-//	QRect rectangle(-m_size / 2, -m_size / 2, m_size, m_size); // x, y, width, height
-//	painter.fillRect(rectangle, painter.brush()); // Fill the rectangle
-//
-//	// Draw the player as a square centered on the origin
-//	painter.setPen(Qt::white);
-//	painter.drawRect(rectangle);
-//
-//	painter.restore();
-//}
 
 Vector2 Player::GetPosition() const
 {
@@ -52,7 +32,6 @@ void Player::UpdatePosition(const Vector2& direction)
 {
 	this->m_position.x += direction.x * m_Character->getSpeed();
 	this->m_position.y += direction.y * m_Character->getSpeed();
-	//std::cout << position;
 }
 
 void Player::UpdatePosition(const float x, const float y)
@@ -61,31 +40,54 @@ void Player::UpdatePosition(const float x, const float y)
 	this->m_position.y += y;
 }
 
-void Player::UpdateRotation(const Vector2& mousePos, const int screenW, const int screenH)
+void Player::UpdateRotation(const Vector2& mousePos)
 {
-	
-	this->m_rotationAngle = CalculateLookAtDirection(mousePos, screenW, screenH).GetAngleFromNormalizedVector();
-	//std::cout << rotationAngle << std::endl;
+	this->m_rotationAngle = CalculateLookAtDirection(mousePos).GetAngleFromNormalizedVector();
 }
 
-void Player::Shoot(const Vector2& mousePosition, const int screenW, const int screenH)
+void Player::Shoot(const Vector2& mousePosition)
 {
-	//Mario aici trebuie returnat input de la sever pt daca trage sau nu
-	//if (m_inputHandler.is_shooting)
-	{
-		Vector2 bulletSpawnPosition = CalculateBulletSpawnPosition();
- 		Vector2 shootDirection = CalculateLookAtDirection(mousePosition, screenW, screenH);
+	Vector2 bulletSpawnPosition = CalculateBulletSpawnPosition();
+ 	Vector2 shootDirection = CalculateLookAtDirection(mousePosition);
 
-		m_weapon.Shoot(bulletSpawnPosition, shootDirection);
-	}
-	m_weapon.Update();
+	m_weapon.Shoot(bulletSpawnPosition, shootDirection);
+}
+
+void Player::Update(float deltaTime)
+{
+	// handles updating the timers on the powerup cooldowns
+	m_weapon.Update(deltaTime);
+}
+
+bool Player::IsAlive() const
+{
+	return m_Character->getHealth() > 0;
+}
+
+void Player::SetScreenSize(const int screenWidth, const int screenHeight)
+{
+	m_screenWidth = screenWidth;
+	m_screenHeight = screenHeight;
+}
+
+crow::json::wvalue Player::toJson() const
+{
+	crow::json::wvalue playerJson;
+	playerJson["playerId"] = m_id;
+	playerJson["x"] = m_position.x;
+	playerJson["y"] = m_position.y;
+	playerJson["hp"] = m_Character->getHealth();
+
+	playerJson["weapon"] = m_weapon.toJson();
+
+	return playerJson;
 }
 
 
-Vector2 Player::CalculateLookAtDirection(const Vector2& mousePos, const int screenW, const int screenH)
+Vector2 Player::CalculateLookAtDirection(const Vector2& mousePos)
 {
-	int mouseOffsetX = mousePos.x - (screenW / 2 - m_position.x);
-	int mouseOffsetY = mousePos.y - (screenH / 2 - m_position.y);
+	int mouseOffsetX = mousePos.x - (m_screenWidth / 2 - m_position.x);
+	int mouseOffsetY = mousePos.y - (m_screenHeight / 2 - m_position.y);
 	m_direction.x = mouseOffsetX - m_position.x;
 	m_direction.y = mouseOffsetY - m_position.y;
 	m_direction.Normalize();
--- TheMonkeyBusyness/TheMonkeyBusyness/Player.h
+++ TheMonkeyBusyness/TheMonkeyBusyness/Player.h
@@ -10,11 +10,11 @@
 #include "Gorilla.h"
 #include "GameObject.h"
 #include "ConstantValues.h"
+
 class Player : public GameObject { // this is the player, he calls for input and other actions
 public:
 	explicit Player(float x=0, float y=0);
 	Vector2 Forward() const;
-	//void draw(QPainter& painter)const;
 	Vector2 GetPosition()const;
 	void SetSpawn(Vector2 location);
 	void SetMonkeyType(Character* character);
@@ -22,14 +22,23 @@ public:
 
 	void UpdatePosition(const Vector2 &vector);
 	void UpdatePosition(const float x, const float y);
-	void UpdateRotation(const Vector2& mousePos, const int screenW, const int screenH);
-	void Shoot(const Vector2& mousePos, const int screenW, const int screenH);
+	void UpdateRotation(const Vector2& mousePos);
+	void Shoot(const Vector2& mousePos);
+	void Update(float deltaTime);
+	bool IsAlive() const;
+
+	void SetScreenSize(const int screenWidth, const int screenHeight);
+
+	crow::json::wvalue toJson() const;
 private:
+	int m_id;
 	Character* m_Character;
 	Vector2 m_position;
 	Vector2 m_direction;
 	float m_rotationAngle{ 0 };
 	float m_size{ kPlayerSize };
-	Vector2 CalculateLookAtDirection(const Vector2& mousePos, const int screenW, const int screenH);
+	int m_screenWidth;
+	int m_screenHeight;
+	Vector2 CalculateLookAtDirection(const Vector2& mousePos);
 	Vector2 CalculateBulletSpawnPosition() const;
 };

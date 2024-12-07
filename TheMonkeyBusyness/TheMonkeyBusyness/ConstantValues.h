b6bbe9a7289be347dfda133c2776bf18b1be01c2
Mario <mariovoicuandrei@gmail.com>
2024-12-07 10:56:50 +0200

    remove useless constant

--- TheMonkeyBusyness/TheMonkeyBusyness/ConstantValues.h
+++ TheMonkeyBusyness/TheMonkeyBusyness/ConstantValues.h
@@ -9,7 +9,7 @@ namespace WeaponConfig
 {
     constexpr float kBasicSpeed = 3;
     constexpr int kBasicDamage = 10;
-    constexpr float kBasicFireRate = 0.1;  //fire rate of 1 means 1 shot every 1s. Fire rate of 0.1 means 10 shots every 1s
+    constexpr float kBasicFireRate = 3;  //fire rate of 1 means 1 shot every 1s. Fire rate of 0.1 means 10 shots every 1s
     constexpr float kMaxBasicBullets = 100;
 
     constexpr float kSpeedPowerupIncreasePercent = 100;
@@ -21,7 +21,5 @@ namespace PlayerConfig
 {
     constexpr int kDefaultPlayeId = -1;
 }
-
-constexpr float frameTime = 1.0f / 60.0f;
 
 #endif // CONSTANTS_H
